980b5eb6c22ed1ce15acc4bd7421bd436fbdaba5
Maciej <maciejfoks22@gmail.com>
2023-05-23 21:05:09 +0200

    yeah

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const firstName = 'Tomek'
+let age = 12
+age = 18
+ 
+let favColor = 'niebieski'
+const favMeal = 'schabowy'
+ 
+let currentCar
+currentCar = 'Audi'
+ 
+let secondName = 'Ania'
+let age2 = 24
+let favColor2 = 'czerwony'
